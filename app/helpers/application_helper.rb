d37d9f59bb7ab4387e6af3cd7208d543544a78a0
omirbakit <omersat@mail.ru>
2022-02-16 06:21:15 +0500

    Add number of questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
   	  asset_path 'avatar.jpg'
   	end
   end
+
+  def inclination(num, vopros, voprosa, voprosov)
+
+    ostatok = num % 10
+    ostatok100 = num % 100
+
+    if  ostatok100 >= 11 && ostatok100 <= 14 || num >= 11 && num <= 14
+      voprosov
+    elsif ostatok == 1 && ostatok100 != 11
+      vopros
+    elsif ostatok >= 2 && ostatok <= 4 || num >= 2 && num <= 4
+      voprosa
+    elsif ostatok > 4 || ostatok == 0
+      voprosov
+    end
+
+  end
 end
