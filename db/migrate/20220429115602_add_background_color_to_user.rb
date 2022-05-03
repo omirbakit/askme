74c64635ea08b271bfffc839f9b0d67a84dd265b
omirbakit <omersat@mail.ru>
2022-05-03 11:35:42 +0500

    Add change users background color

--- db/migrate/20220429115602_add_background_color_to_user.rb
+++ db/migrate/20220429115602_add_background_color_to_user.rb
@@ -1,5 +1,5 @@
 class AddBackgroundColorToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :background_color, :string
+    add_column :users, :background_color, :string, default: '#005a55'
   end
 end
