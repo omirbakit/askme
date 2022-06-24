3b76fedc2877fae184158c40247f2b9e761e4165
omirbakit <omersat@mail.ru>
2022-06-24 16:17:00 +0500

    Add question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,8 @@
 class Question < ApplicationRecord
-
+	has_many :hashtag_questions, dependent: :destroy
+	has_many :hashtags, through: :hashtag_questions
 	belongs_to :user
-	belongs_to :author, class_name: 'User'
+	belongs_to :author, class_name: 'User', optional: true
 
-	validates :text, :user, presence: true
-	validates :text, length: { maximum: 255 }
-
+	validates :text, presence: true, length: { maximum: 255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_115040) do
+ActiveRecord::Schema.define(version: 2022_06_24_105819) do
+
+  create_table "hashtag_questions", force: :cascade do |t|
+    t.integer "hashtag_id", null: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hashtag_id"], name: "index_hashtag_questions_on_hashtag_id"
+    t.index ["question_id"], name: "index_hashtag_questions_on_question_id"
+  end
 
   create_table "hashtags", force: :cascade do |t|
     t.string "text", null: false
@@ -42,5 +51,7 @@ ActiveRecord::Schema.define(version: 2022_06_17_115040) do
     t.string "background_color", default: "#005a55"
   end
 
+  add_foreign_key "hashtag_questions", "hashtags"
+  add_foreign_key "hashtag_questions", "questions"
   add_foreign_key "questions", "users"
 end
